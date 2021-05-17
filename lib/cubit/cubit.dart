70da084dcc8c40462738ee3ce752eb5e7dd5758a
abanoub <abanoub_samy123@yahoo.com>
2021-05-17 15:19:54 +0200

    create_ScreensAndCubit

---
+++ lib/cubit/cubit.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/cubit/states.dart';
+import 'package:news_app/screens/business_screen.dart';
+import 'package:news_app/screens/science_screen.dart';
+import 'package:news_app/screens/sport_screen.dart';
+
+class AppCubit extends Cubit<AppStates> {
+  AppCubit() : super(InitialState());
+
+  static AppCubit get(context) => BlocProvider.of(context);
+
+  int currentIndex = 0;
+
+  List<BottomNavigationBarItem> bottomItems = [
+    BottomNavigationBarItem(
+      icon: Icon(Icons.business),
+      label: 'Business',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.sports_volleyball_rounded),
+      label: 'Sport',
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(Icons.science),
+      label: 'Science',
+    ),
+  ];
+  List<Widget> screens = [
+    BusinessScreen(),
+    SportScreen(),
+    ScienceScreen(),
+  ];
+
+  void changeBottomNavBar(index) {
+    currentIndex = index;
+    emit(NewBottomNavState());
+  }
+}
